16d13652a13457ae515705a0dead388896ec1e4d
francois dutheil <frdutheil@yahoo.fr>
2019-02-13 17:27:22 +0100

    to be continued

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/shownew'
+  get 'reviews/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :create, :show]
+    resources :reviews, only: [:new, :create]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'chez tita',
+    address:      '7 Boundary St, Bastia E2 7JE',
+    category:  'italian',
+      },
+  {
+    name:         'chez toto',
+    address:      '56A Shoreditch High St, Toulouse E1 6PQ',
+    category:  'chinese',
+  },
+  {
+    name:         'chez Umami Bros',
+    address:      '7 Boundary St, Honk kong E2 7JE',
+    category:  'japanese',
+      },
+  {
+    name:         'chez Eddy',
+    address:      '7 Boundary St, Paris E2 7JE',
+    category:  'belgian',
+      },
+  {
+    name:         'chez Ginette',
+    address:      '7 Boundary St, Marseille E2 7JE',
+    category:  'french',
+      },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
